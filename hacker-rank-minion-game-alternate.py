43831d97990989f3bd79679dccbacea0cd6be0b8
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-01 19:33:23 +0000

    minion game alternate solution

--- hacker-rank-minion-game-alternate.py
+++ hacker-rank-minion-game-alternate.py
@@ -3,20 +3,17 @@ def minion_game(S):
     s1=0
     s2=0
     vow='AEIOU'
-    for i in range(len(s)):
-        if s[i] not in vow:
-            s1=s1+(len(s)-i)
+    for i in range(len(S)):
+        if S[i] not in vow:
+            s1=s1+(len(S)-i)
         else:
-            s2=s2+(len(s)-i)   
+            s2=s2+(len(S)-i)   
     if s1>s2:
         print("Stuart",s1)
     elif s2>s1:
         print("Kevin",s2)
     else:
         print("Draw")    
-    if __name__ == '__main__':
-        s = input()
-        minion_game(s)
 
 
 if __name__ == '__main__':
