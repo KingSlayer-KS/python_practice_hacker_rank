d633016c6dd4eb0b693d0de6242107dcb54df28a
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-07 21:22:31 +0530

    Solution for all(),any()

--- hacker-rank-all(),any().py
+++ hacker-rank-all(),any().py
@@ -1,11 +1,7 @@
 f,u =int(input()), list(map(str,input().split()))
-<<<<<<< HEAD
-l=[(all(int(i)>0 for i in u) and all(int(i)!=0 for i in u)),((any((i[0:]==i[-1::-1] for i in (u)))))]
-print(all(l))
-=======
 k=[(all(int(i)>0 for i in u) and all(int(i)!=0 for i in u)),((any((i[0:]==i[-1::-1] for i in (u)))))]
 print(all(k))
->>>>>>> 200ae3d642809431016b0d60c7dac5406e1b37fd
+
 
 
 
