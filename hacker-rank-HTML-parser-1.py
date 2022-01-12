742888d386bcbc6cead765caf7ca3fa961756123
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-12 18:14:28 +0530

    Solution HTML parser1

---
+++ hacker-rank-HTML-parser-1.py
@@ -0,0 +1,17 @@
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start : {0}".format(tag))
+        for key, val in attrs:
+            print("-> {0} > {1}".format(key, val))
+    def handle_endtag(self, tag):
+        print("End   : {0}".format(tag))
+    def handle_startendtag(self, tag, attrs):
+        print("Empty : {0}".format(tag))
+        for key, val in attrs:
+            print("-> {0} > {1}".format(key, val))
+parser = MyHTMLParser()
+n = int(input())
+for i in range(n):
+    parser.feed(input())
+
