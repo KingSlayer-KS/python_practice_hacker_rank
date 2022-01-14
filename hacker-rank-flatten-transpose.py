3caffc7379a6eff9a3bb20da0f76cbc0d3c3636c
Kingslayer <sirjanssk2933@protonmail.ch>
2022-01-14 17:03:01 +0530

    Solution for flatten transpose

---
+++ hacker-rank-flatten-transpose.py
@@ -0,0 +1,17 @@
+
+import re
+import numpy
+
+def arrays(k):
+    f= numpy.array(k,int)
+    c = f.flatten()
+    return("{0}\n{1}".format(numpy.transpose(f),c)) 
+    
+
+k=[]
+l= list(map(int,input().split()))
+for i in range (l[0]):
+    k.append(input().strip().split(' '))
+result= arrays(k)
+print(result)
+
