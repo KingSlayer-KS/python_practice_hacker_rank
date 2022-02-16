2de1e5fbddc97ad6529ff7e9f72607427af2c933
KingSlayer-KS <72351791+KingSlayer-KS@users.noreply.github.com>
2022-02-16 20:03:32 +0000

    solution for .issubset()

--- hacker-rank-.issubset().py
+++ hacker-rank-.issubset().py
@@ -1,5 +1,5 @@
 f= int(input())
 for i in range(f):
     u ,c =int(input()), set(map(int,input().split()))
-    b ,k = int(input()), set(map(int,input().split()))
+    m ,k = int(input()), set(map(int,input().split()))
     print(c.issubset(k))
